bed77dff03eacfd0d78a69d2cb5cc950dfcd0d9c
osamanazar47 <osamanazar47@gmail.com>
2023-11-14 00:48:14 -0800

    fix

--- _getenv.c
+++ _getenv.c
@@ -5,25 +5,12 @@
  */
 void print_env(void)
 {
-	char **env = environ, *key;
-	size_t count = 0, j, i , n;
+	char **env = environ;
 
-	while (env[count] != NULL)
-		count++;
-	for (i = 1; i < count; i++)
+	while (*env != NULL)
 	{
-		key = env[i];
-		j = i;
-		while (j > 0 && _strcmp(key, env[j - 1]) < 0)
-		{
-			env[j] = env[j - 1];
-			j--;
-		}
-		env[j] = key;
-	}
-	for (n = 0; n < count; n++)
-	{
-		_puts(env[n]);
+		_puts(*env);
 		_putchar('\n');
+		env++;
 	}
 }
